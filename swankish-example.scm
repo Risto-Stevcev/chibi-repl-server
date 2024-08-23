22d58124285f884e8a8e0333fe3de4ff7d39f8fe
Risto Stevcev <me@risto.codes>
2024-08-23 12:17:17 +0200

    Added non-blocking mode example

--- swankish-example.scm
+++ swankish-example.scm
@@ -1,9 +1,16 @@
 (import (scheme base)
         (scheme write))
-(import (swankish-server))
 
 (define (foo x y) (+ x y))
 ;; Try C-x C-e on a (foo 3 4) form in geiser
 
-(define-and-start-repl-server 5556)
-
+;; Start the swankish server
+(import (srfi 18)
+        (swankish-server))
+(let ((server-thread (make-thread
+                      (lambda ()
+                        (define-and-start-repl-server 5556)))))
+  (thread-start! server-thread)
+  (display "Swankish server is running in non-blocking mode.\n")
+  (thread-join! server-thread)
+  (display "Swankish server exited.\n"))
